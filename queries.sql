46005f467a95c5de6cfed09e22e6e72ebd896a78
rkliver <kliver.one.90@gmail.com>
2023-03-20 16:38:12 +0500

    создал файл queries.sql

---
+++ queries.sql
@@ -0,0 +1,5 @@
+INSERT INTO projects SET title = 'Входящие';
+INSERT INTO projects SET title = 'Учеба';
+INSERT INTO projects SET title = 'Работа';
+INSERT INTO projects SET title = 'Домашние дела';
+INSERT INTO projects SET title = 'Авто';
